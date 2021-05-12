0c4171a244e75fcb6b69cacd7f97dbfb66305388
Tyler <tyler_venetsanos@msn.com>
2021-05-12 09:42:45 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -8,12 +8,20 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
-  
+
+  handleClick = () => {
+    const newColor = this.props.getSelectedColor()
+    this.setState({
+      color: newColor
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell"
+           style={{backgroundColor: this.state.color}}
+           onClick={this.handleClick}>
       </div>
     )
   }
-  
-}
+}  
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,8 +4,8 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      let callback = () => { this.props.setSelectedColor(str) }
-      return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const callback = () => { this.props.setSelectedColor(str) }
+      return <div key={idx} onClick={callback} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
 
